74306ecbf6d16abc891fd08c731aa7777e33f94e
cintiaohara <cintia.ohara@sptech.school>
2024-08-31 06:44:59 -0300

    primeira atualização do site institucional

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  handleScroll(); 
+};
+
+window.onscroll = function() {
+  handleScroll();
+};
+
+function handleScroll() {
+  var header = document.getElementById("header");
+  if (window.pageYOffset > 50) {
+      header.style.backgroundColor = "#3a1b1be6";
+  } else {
+      header.style.backgroundColor = "transparent";
+  }
+}
+
+AOS.init();
