fd7219da45c6a30348309e3e873f6bb43e587c80
Brugoamorim <bruno.asilva@sptech.school>
2024-10-06 20:35:40 -0300

    subindo funcao login

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,11 +1,7 @@
 var database = require("../database/config")
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    var instrucaoSql = `SELECT * FROM Usuario WHERE email = "${email}" AND senha = "${senha}"`;
     return database.executar(instrucaoSql);
 }
 
@@ -17,12 +13,18 @@ function cadastrar(nome, email, senha, telefone, idEmpresa) {
 }
 
 function buscarEmail(email){
-    var instrucaoSql = `select * from Usuario where email = "${email}"`;
+    var instrucaoSql = `SELECT * FROM Usuario WHERE email = "${email}"`;
+    return database.executar(instrucaoSql);
+}
+
+function buscarUsuario(email, senha){
+    var instrucaoSql = `SELECT * FROM Usuario WHERE email = "${email}" AND senha = "${senha}"`;
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
     autenticar,
     cadastrar,
-    buscarEmail
+    buscarEmail,
+    buscarUsuario
 };
