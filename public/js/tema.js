01b1f4c9d19146864a19d0a9de3cb0e8f7f2eec5
biancas0uz4 <bianca.rsouza@sptech.school>
2024-12-01 20:11:26 -0300

    checkbox tema da dash

--- public/js/tema.js
+++ public/js/tema.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('themeSwitch');
     const body = document.body;
 
+    const savedTheme = sessionStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.toggle('light-theme', savedTheme === 'light');
+        themeToggle.checked = savedTheme === 'light'; 
+    }
+
     themeToggle.addEventListener('click', () => {
         body.classList.toggle('light-theme');
+        
+        const newTheme = body.classList.contains('light-theme') ? 'light' : 'dark';
+        sessionStorage.setItem('theme', newTheme);
     });
 });
